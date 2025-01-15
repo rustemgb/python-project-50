2fb7568b651725e4caad9b294a96a11c846343b2
Rustem <rustemkago12345@gmail.com>
2025-01-15 10:59:53 +0300

    fix gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,11 @@
 from gendiff.cli import cli_parser
+from gendiff.generate_diff import generate_diff
 
 
 def main():
-    cli_parser()
+    first_file, second_file = cli_parser()
+    diff = generate_diff(first_file, second_file)
+    print(diff)
 
 
 if __name__ == "__main__":
