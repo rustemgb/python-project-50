2962c45cedef6fd044f4a4e211040c5dd36fb64d
Rustem <rustemkago12345@gmail.com>
2025-01-20 09:29:50 +0300

    fix

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -4,28 +4,25 @@ import pytest
 
 from gendiff.generate_diff import generate_diff
 
-DATA_DIR = os.path.join(os.path.dirname(__file__), "test_data")
+TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "test_data")
 
 
 def read_fixture(file_name):
-    with open(os.path.join(DATA_DIR, file_name), "r") as file:
-        return file.read().strip()
+    with open(os.path.join(TEST_DATA_DIR, file_name), "r") as file:
+        return file.read()
 
 
 @pytest.mark.parametrize(
     "file1, file2, result",
     [
         (
-            "file1.json", "file1.json", "result.txt"
+            "file1.json", "file2.json", "result.txt"
         )
     ],
 )
 def test_generate_diff(file1, file2, result):
 
-    file1_path = os.path.join(DATA_DIR, file1)
-    file2_path = os.path.join(DATA_DIR, file2)
+    file1_path = os.path.join(TEST_DATA_DIR, file1)
+    file2_path = os.path.join(TEST_DATA_DIR, file2)
 
-    expected = read_fixture(result)
-
-    diff = generate_diff(file1_path, file2_path)
-    assert diff == expected
+    assert generate_diff(file1_path, file2_path) == read_fixture(result)
