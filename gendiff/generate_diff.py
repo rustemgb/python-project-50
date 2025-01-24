0a43c8c5e91b807375d9f17df36c9892fcebfc6d
Rustem <rustemkago12345@gmail.com>
2025-01-24 07:40:58 +0300

    fix import

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,8 +1,8 @@
-from gendiff.gendiff import gendiff
-from gendiff.reading_files import reading_files
+from gendiff.generator import generator
+from gendiff.parser_files import parser_data_from_file
 
 
 def generate_diff(file_path1, file_path2):
-    file1 = reading_files(file_path1)
-    file2 = reading_files(file_path2)
-    return gendiff(file1, file2)
+    file1 = parser_data_from_file(file_path1)
+    file2 = parser_data_from_file(file_path2)
+    return generator(file1, file2)
