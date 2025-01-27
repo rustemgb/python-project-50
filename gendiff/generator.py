3f2d06f472f2e280a1b53a807c3ccafdd4684e31
Rustem <rustemkago12345@gmail.com>
2025-01-27 12:33:52 +0300

    add logic status

--- gendiff/generator.py
+++ gendiff/generator.py
@@ -1,3 +1,36 @@
+def get_added_status(key, value):
+    return {
+        'status': 'added',
+        'key': key,
+        'value': value,
+    }
+
+
+def get_deleted_status(key, value):
+    return {
+        'status': 'deleted',
+        'key': key,
+        'value': value,
+    }
+
+
+def get_changed_status(key, value1, value2):
+    return {
+        'status': 'changed',
+        'key': key,
+        'old_value': value1,
+        'new_value': value2,
+    }
+
+
+def get_unchanged_status(key, value):
+    return {
+        'status': 'unchanged',
+        'key': key,
+        'value': value,
+    }
+
+
 def generator(file1, file2):
 
     all_key = sorted(file1.keys() | file2.keys())
@@ -10,15 +43,13 @@ def generator(file1, file2):
         value2 = file2.get(key)
 
         if key in added:
-            result.append(f"  + {key}: {value2}")
+            result.append(get_added_status(key, value2))
         elif key in deleted:
-            result.append(f"  - {key}: {value1}")
-
+            result.append(get_deleted_status(key, value1))
         else:
             if value1 != value2:
-                result.append(f"  - {key}: {value1}")
-                result.append(f"  + {key}: {value2}")
+                result.append(get_changed_status(key, value1, value2))
             else:
-                result.append(f"    {key}: {value2}")
-                
-    return ('{\n' + '\n'.join(result) + '\n}').lower()
+                result.append(get_unchanged_status(key, value1))
+    
+    return result
