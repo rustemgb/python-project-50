3223cb988ea2eb52022fee6e8f052175874d683d
Rustem <rustemkago12345@gmail.com>
2025-01-31 12:53:43 +0300

    add converting value

--- gendiff/stylish.py
+++ gendiff/stylish.py
@@ -1,23 +1,49 @@
-def get_diff_format_stylish(files_with_status):
-    
-    result = []
-    for file in files_with_status:
+def converting_value(value, indent_count=2):
+    if isinstance(value, bool):
+        return str(value).lower()
+    if value == None:
+        return 'null'
+    if isinstance(value, dict):
+        indent = " " * (indent_count + 4)
+        result = []
 
-        old_value = str(file.get('old_value'))
-        new_value = str(file.get('new_value'))
-        value = str(file.get('value'))
-        key = str(file.get('key'))
-        status = file.get('status')
+        for key, inner_value in value.items():
+            convert_value = converting_value(inner_value, indent_count + 4)
+            result.append(f'{indent}  {key}: {convert_value}')
+
+        string_format = '\n'.join(result)
+        end_indent = " " * (indent_count + 2)   
+        return f"{{\n{string_format}\n{end_indent}}}"
+    return f'{value}'
+    
+
+def get_diff_format_stylish(diff, indent_count=2):
+
+    indent = " " * indent_count
+    result = []
+
+    for item in diff:
+        old_value = converting_value(item.get('old_value'), indent_count)
+        new_value = converting_value(item.get('new_value'), indent_count)
+        value = converting_value(item.get('value'), indent_count)
+
+        key = item.get('key')
+        status = item.get('status')
 
         if status == 'added':
-            result.append(f'  + {key}: {value}')
+            result.append(f'{indent}+ {key}: {value}')
         if status == 'deleted':
-            result.append(f'  - {key}: {value}')
+            result.append(f'{indent}- {key}: {value}')
         if status == 'changed':
-            result.append(f'  - {key}: {old_value}')
-            result.append(f'  + {key}: {new_value}')
+            result.append(f'{indent}- {key}: {old_value}')
+            result.append(f'{indent}+ {key}: {new_value}')
         if status == 'unchanged':
-            result.append(f'    {key}: {value}')
+            result.append(f'{indent}  {key}: {value}')
+        if status == 'nested':
+            children = get_diff_format_stylish(
+                item.get('children'), indent_count + 4)
+            result.append(f'{indent}  {key}: {children}')
         
-    return ('{\n' + '\n'.join(result) + '\n}').lower()
-
+    string_format = '\n'.join(result)
+    end_indent = " " * (indent_count - 2)    
+    return f"{{\n{string_format}\n{end_indent}}}"
