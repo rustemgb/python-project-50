f2e258cc8f51f4cf3bf515ff424eaf845142c069
Rustem <rustemkago12345@gmail.com>
2025-02-07 15:06:59 +0300

    add formater

---
+++ gendiff/formater/formater.py
@@ -0,0 +1,9 @@
+from gendiff.formater.stylish import get_diff_format_stylish
+from gendiff.formater.plain import format_plain
+
+
+def choice_formats(diff, formatter):
+    if formatter == "stylish":
+        return get_diff_format_stylish(diff)
+    if formatter == "plain":
+        return format_plain(diff)
