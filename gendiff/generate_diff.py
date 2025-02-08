10d043becc41229ac69b455407ba86f2242b3f41
Rustem <rustemkago12345@gmail.com>
2025-02-08 07:43:09 +0300

    fix import

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,6 +1,6 @@
+from gendiff.formater.formater import choice_formats
 from gendiff.generator import generator
 from gendiff.parser_files import parser_data_from_file
-from gendiff.formater.formater import choice_formats
 
 
 def generate_diff(file_path1, file_path2, formatter="stylish"):
