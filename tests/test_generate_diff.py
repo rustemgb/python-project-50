f8894ee6431e2f5a5fa4f3181460ac917b4a7173
Rustem <rustemkago12345@gmail.com>
2025-02-10 10:08:06 +0300

    fix code

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -23,12 +23,12 @@ def read_fixture(file_name):
         ("file1.yml", "file2.yml", "json", "result_json.txt"),
     ],
 )
-def test_generate_diff(file1, file2, formatter, file):
+def test_generate_diff(file1, file2, formatter, result_file):
 
     file1_path = os.path.join(TEST_DATA_DIR, file1)
     file2_path = os.path.join(TEST_DATA_DIR, file2)
 
-    correct_result = read_fixture(file)
+    correct_result = read_fixture(result_file)
     result = generate_diff(file1_path, file2_path, formatter)
 
     assert result == correct_result
